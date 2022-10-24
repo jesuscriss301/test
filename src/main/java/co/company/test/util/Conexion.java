5dfc4ce938038be3603bebd28d6555003533ad9d
Jesus Cristancho <jesusmanuelcp@ufps.edu.co>
2022-10-24 17:32:13 -0500

    metodo cerrarConexion()

--- src/main/java/co/company/test/util/Conexion.java
+++ src/main/java/co/company/test/util/Conexion.java
@@ -3,32 +3,41 @@ package co.company.test.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.concurrent.ExecutionException;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author jesus
  */
 public class Conexion {
-    
+
     private Connection con = null;
     private PreparedStatement preparedStatement;
-    
+
     private static final String url = "jdbc:mysql://localhost:3306/";
     private static final String bdName = "test";
     private static final String driver = "com.mysql.jdbc.Driver";
     private static final String username = "root";
     private static final String password = "";
-    
-    public Conexion(){
-        try{
+
+    public Conexion() {
+        try {
             Class.forName(driver);
-            con = (Connection) DriverManager.getConnection(url+bdName, username, password);
-        }catch(Exception e){
+            con = (Connection) DriverManager.getConnection(url + bdName, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cerrarConexion() {
+
+        try {
+            con.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
